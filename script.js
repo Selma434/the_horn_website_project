e431b6c03674e9afe2de6da30be64c32f37b6f8f
Selma Yussuf <selmayussuf@selmas-mbp.home>
2025-11-02 19:29:29 +0000

    adding files to repo

---
+++ script.js
@@ -0,0 +1,31 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    var submitbutton=document.getElementById("submitbutton");
+
+    submitbutton.addEventListener('click', () => {
+        alert("Thank you for your feedback!");
+    } );
+    
+    burger.addEventListener('click' , () => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+        
+        //Animate Links
+        navLinks.forEach((link, index) => {
+           if(link.style.animation) {
+                link.style.animation = '';
+            } else {
+               link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
+            }
+            console.log(index / 7);
+        });
+        //Burger Animation
+        burger.classList.toggle('toggle');
+    });   
+
+}
+
+navSlide();
+
